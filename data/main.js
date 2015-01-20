78d9c776f8d196a992ba806ea65dce14872efae9
randomdude999 <peeter.a.randlaa@gmail.com>
2015-01-20 07:30:31 +0200

    FIXED EVERYTHING
    Yes I did... Exept I never actually fixed the pause issue

--- data/main.js
+++ data/main.js
@@ -1,3 +1,45 @@
+var canvas = document.getElementById('canvas');
+var ctx = document.getElementById('canvas').getContext('2d');
+
+canvas.width = document.documentElement.clientWidth - 2;
+canvas.height = document.documentElement.clientHeight - 5;
+
+
+ 
+
+var mySprite = {
+
+    x: Math.random() * canvas.width,
+
+    y: Math.random() * canvas.height,
+
+    size: 50,
+
+    speed: 350,
+
+    color: '#0ff'
+
+};
+
+ 
+
+var item = {
+
+    x: Math.round(Math.random() * canvas.width),
+
+    y: Math.round(Math.random() * canvas.height),
+
+    size: 10,
+
+    color: '#f00'
+
+};
+
+
+
+var dev = true;
+
+
 var itemCounter = 0;
 var timer = 0;
 var paused = false;
@@ -41,8 +83,8 @@ function update(mod) {
     ) {
     item.x = Math.round(Math.random() * canvas.width),
     item.y = Math.round(Math.random() * canvas.height),
-        itemCounter ++;
-        itemNotInCanvas();
+    itemCounter ++;
+    itemNotInCanvas();
     }
     
     if (mySprite.x < 0) {
